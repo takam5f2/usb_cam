6e8cc81c438193298e4bef6a539102271e4df369
Takayuki AKAMINE <takayuki.akamine@tier4.jp>
2022-09-26 23:17:51 +0900

    chore: change lifespan to 100 msec

--- src/usb_cam_node.cpp
+++ src/usb_cam_node.cpp
@@ -44,7 +44,7 @@ UsbCamNode::UsbCamNode(const rclcpp::NodeOptions & node_options)
   img_(new sensor_msgs::msg::Image()),
   image_pub_(std::make_shared<image_transport::CameraPublisher>(
       image_transport::create_camera_publisher(this, "image_raw",
-      rclcpp::QoS(10).get_rmw_qos_profile()))),
+      rclcpp::QoS(10).lifespan(std::chrono::milliseconds(100)).get_rmw_qos_profile()))),
   service_capture_(
     this->create_service<std_srvs::srv::SetBool>(
       "set_capture",
